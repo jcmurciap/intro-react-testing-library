a37cff8859ce76b943f640b86b62d6d172e1561a
enigma <camilo.murcia.pimentel@gmail.com>
2022-05-31 23:30:48 -0500

    Added trigger events in syncronous test

---
+++ src/components/counter/counter.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render, screen,fireEvents, fireEvent} from '@testing-library/react';
+
+import Counter from './counter';
+
+test('display zero initial counts', () => {
+    
+    render(<Counter />);
+    const result = screen.getByText(/clicked times: 0/i);
+    expect(result).toBeInTheDocument();
+})
+
+test('Display new counter after one click', () => {
+    render(<Counter />);
+
+    const button = screen.getByText(/click/i, {selector: 'button'});
+    fireEvent.click(button);
+    const result = screen.getByText(/clicked times: 1/i);
+    expect(result).toBeInTheDocument();
+
+})
+
